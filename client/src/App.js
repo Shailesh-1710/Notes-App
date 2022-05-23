681b70cc9a4b0aad4deb6034480ab877537b052b
Shailesh <shailupatel17@gmail.com>
2022-05-23 17:08:48 +1000

    sever and client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
--- server/routers/notesRouters.js
+++ server/routers/notesRouters.js
@@ -1,9 +1,38 @@
 const router = require("express").Router();
+var axios = require("axios");
+require("dotenv").config();
 
-router.post("/", (req, res) => {
-  const body = req.body;
-  const { name, description } = req.body;
-  console.log(name);
+console.log(process.env.MONGO);
+
+// router.post("/", (req, res) => {
+//   const body = req.body;
+//   const { name, description } = req.body;
+//   console.log(body.name);
+// });
+var data = JSON.stringify({
+  collection: "notes",
+  database: "NotesDB",
+  dataSource: "Cluster0",
+  filter: { name: "John Sample" },
 });
+
+var config = {
+  method: "post",
+  url: `${process.env.DELETEONE}`,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Request-Headers": "*",
+    "api-key": `${process.env.APIKEY}`,
+  },
+  data: data,
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 module.exports = router;
