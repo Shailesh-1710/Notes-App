e398b6fe1268502f1cc53355780a616553609a7e
Shailesh <shailupatel17@gmail.com>
2022-06-07 22:00:21 +1000

    Frontend Srtup.

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const axios = require("axios");
 
 export default function Home() {
+  const [dataArray, setDataArray] = useState();
+
+  async function getNotes() {
+    const fetcheddata = await axios.get("http://localhost:5000/");
+    setDataArray(fetcheddata.data.documents);
+    console.log(fetcheddata.data.documents);
+  }
+
+  // function renderObj() {
+  //   Object.keys(dataArray).map((obj, i) => {
+  //     return <div>{dataArray[obj].Title}</div>;
+  //   });
+  // }
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
   return (
     <div>
       <h1>Home Component</h1>
+      <h2>DATA IS IN {typeof dataArray} From</h2>
+      {/* <div>{renderObj}</div> */}
+      {/* <div>
+        {dataArray.map(({ Title }) => (
+          <p>TITLE IS : {Title} </p>
+        ))}
+      </div> */}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(5000, () => {
   console.log("Server Stateted on port 5000");
